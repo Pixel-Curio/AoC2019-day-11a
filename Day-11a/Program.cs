15344df5dbcb3f02154901831b5817f6524e7446
Michael Sanders <mike.gamedev@gmail.com>
2019-12-10 23:01:35 -0800

    Updated to solve part B of day 11.

--- Day-11a/Program.cs
+++ Day-11a/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Day_11a
 {
@@ -18,13 +19,14 @@ namespace Day_11a
     class Bot
     {
         private Dictionary<(long x, long y), long> _panels = new Dictionary<(long, long), long>();
-        private (long x, long y) _position;
-        private Direction _direction = Direction.Up;
+        private (long x, long y) _position = (0,0);
+        private Direction _direction = Direction.Down;
         private Intcode _processor;
 
         public Bot(long[] code)
         {
             _processor = new Intcode(code);
+            _panels.Add(_position, 1);
 
             (bool, long) result;
             do
@@ -33,16 +35,31 @@ namespace Day_11a
 
                 result = _processor.Process(new List<long> { _panels[_position] });
                 _panels[_position] = result.Item2;
+                if (result.Item1) break;
 
-                result = _processor.Process(null);
-                if (result.Item2 == 0) TurnLeft();
-                else if (result.Item2 == 1) TurnRight();
+                result = _processor.Process(new List<long> { _panels[_position] });
+                if (result.Item2 == 0) TurnRight();
+                else if (result.Item2 == 1) TurnLeft();
                 else throw new Exception($"Invalid return value received from brain: {result.Item2}");
 
                 Move();
             } while (!result.Item1);
 
             Console.WriteLine($"Finished processing. Panels touched: {_panels.Count}");
+            Console.WriteLine($"Min x:{_panels.OrderBy(x => x.Key.x).First().Key.x} Max x:{_panels.OrderByDescending(x => x.Key.x).First().Key.x}");
+            Console.WriteLine($"Min y:{_panels.OrderBy(x => x.Key.y).First().Key.y} Max y:{_panels.OrderByDescending(x => x.Key.y).First().Key.y}");
+
+            string[] identifier = new string[8];
+            for (int i = 0; i < identifier.Length; i++) identifier[i] = "".PadLeft(50, ' ');
+
+            foreach (var panel in _panels)
+            {
+                StringBuilder builder = new StringBuilder(identifier[panel.Key.y]);
+                builder[(int)panel.Key.x] = panel.Value == 1 ? '8' : ' ';
+                identifier[panel.Key.y] = builder.ToString();
+            }
+
+            foreach (string str in identifier) Console.WriteLine(str);
         }
 
         private void TurnRight() => _direction = (int)++_direction > 3 ? 0 : _direction;
